6ce3466083660f208c95f76b78820cfa4948db83
me1ze <me1ze@proton.me>
2026-03-30 00:02:44 +0700

    menambahkan class DataGeneric

--- Program.cs
+++ Program.cs
@@ -6,10 +6,29 @@ using System.Threading.Tasks;
 
 namespace TP_MODUL5_103022400070
 {
+    class DataGeneric<T>
+    {
+        private T data;
+
+        public DataGeneric(T data)
+        {
+            this.data = data;
+        }
+
+        public void PrintData()
+        {
+            Console.WriteLine("Data yang tersimpan adalah: " + data);
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
+            DataGeneric<string> data = new DataGeneric<string>("103022400070");
+            data.PrintData();
+
+            Console.ReadLine();
         }
     }
 }
