543c1316b05ac19d25267de2e513c9ac2b8f40ee
me1ze <me1ze@proton.me>
2026-03-29 23:44:09 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_MODUL5_103022400070
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
