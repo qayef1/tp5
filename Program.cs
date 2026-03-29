024092c25aae37c0ea88cc173e0ba1ec38c1514b
me1ze <me1ze@proton.me>
2026-03-29 23:53:38 +0700

    commit message

--- Program.cs
+++ Program.cs
@@ -6,10 +6,22 @@ using System.Threading.Tasks;
 
 namespace TP_MODUL5_103022400070
 {
+    class HaloGeneric
+    {
+        public void SapaUser<T>(T user)
+        {
+            Console.WriteLine("Halo user " + user);
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
+            HaloGeneric halo = new HaloGeneric();
+            halo.SapaUser<string>("priasolo67");
+
+            Console.ReadLine();
         }
     }
 }
